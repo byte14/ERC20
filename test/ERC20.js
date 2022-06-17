a3dcc1d6f2ee3dfb77b2707b13f46458700c28d0
byte14 <avishekrajpanta@gmail.com>
2022-06-17 17:41:46 +0545

    Added tests for approve function

--- test/ERC20.js
+++ test/ERC20.js
@@ -46,23 +46,23 @@ describe("ERC20 Contract", () => {
 
   describe("transfer", () => {
     it("transfer tokens from owner account to bob account", async () => {
-      await contract.transfer(bob.address, ethers.utils.parseEther('300'));
+      await contract.connect(owner).transfer(bob.address, ethers.utils.parseEther('300'));
       expect(await contract.balanceOf(bob.address)).to.equal(ethers.utils.parseEther('300'));
       expect(await contract.balanceOf(owner.address)).to.equal(ethers.utils.parseEther('700'));
     });
 
     it("fire 'Tansfer' event when sucessfully transfer token ownership", async () => {
-    const transferTx = await contract.transfer(bob.address, ethers.utils.parseEther('300'));
+    const transferTx = await contract.connect(owner).transfer(bob.address, ethers.utils.parseEther('300'));
     await expect(transferTx).to.emit(contract, 'Transfer').withArgs(owner.address, bob.address, ethers.utils.parseEther('300'))
     });
 
     it("throws when transfer to the zero address", async () => {
-      const transferTx = contract.transfer(ethers.constants.AddressZero, ethers.utils.parseEther('300'));
+      const transferTx = contract.connect(owner).transfer(ethers.constants.AddressZero, ethers.utils.parseEther('300'));
       await expect(transferTx).to.be.revertedWith("Transfer to the zero address");
     });
 
     it("throws when transfer more token than owned", async () => {
-      const transferTx = contract.transfer(bob.address, ethers.utils.parseEther('2000'));
+      const transferTx = contract.connect(owner).transfer(bob.address, ethers.utils.parseEther('2000'));
       await expect(transferTx).to.be.revertedWith("Insufficient tokens!");
     });
   });
@@ -96,6 +96,23 @@ describe("ERC20 Contract", () => {
       await contract.connect(owner).approve(bob.address, ethers.utils.parseEther('100'))
       const transferTx = contract.connect(bob).transferFrom(owner.address, alice.address, ethers.utils.parseEther('200'));
       await expect(transferTx).to.be.revertedWith("Insufficient allowance!");
+    });
+  });
+
+  describe("approve", () => {
+    it("approves bob account to spend 100 tokens on owner account behalf", async () => {
+      await contract.connect(owner).approve(bob.address, ethers.utils.parseEther('100'));
+      expect(await contract.allowance(owner.address, bob.address)).to.equal(ethers.utils.parseEther('100'));
+    });
+
+    it("fire 'Approval' event when successfully called approve function", async () => {
+      const approveTx = await contract.connect(owner).approve(bob.address, ethers.utils.parseEther('100'));
+      await expect(approveTx).to.emit(contract, 'Approval').withArgs(owner.address, bob.address, ethers.utils.parseEther('100'));
+    });
+
+    it("throws when approve to the zero address", async () => {
+      const approveTx = contract.connect(owner).approve(ethers.constants.AddressZero, ethers.utils.parseEther('100'));
+      await expect(approveTx).to.be.revertedWith("Approve to the zero address!");
     });
   });
 });
