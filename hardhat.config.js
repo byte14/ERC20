4c6f79a06ef1abbfc72867452dfbb1c840f1d0fc
byte14 <avishekrajpanta@gmail.com>
2022-06-23 20:54:27 +0545

    Added Etherscan API

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+
+const { INFURA_API_KEY } = process.env;
+const { PRIVATE_KEY } = process.env;
+const { ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.7",
   networks: {
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+      url: INFURA_API_KEY,
+      accounts: PRIVATE_KEY
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
